1bb7db2fafcd7cfebf2fca75c79d5f3fb3dfcead
Pratik Kale <ppvkale@gmail.com>
2024-01-16 13:30:52 +0530

    Task Get block number

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,28 @@
 const { ethers, run, network } = require("hardhat");
 
 async function main() {
+  //Deploy the contract
   const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
   console.log("Deploying Contract...");
   const simpleStorage = await SimpleStorageFactory.deploy();
   console.log("Contract deployed to:", simpleStorage.target);
 
+  //Verify the contract
   if (network.config.chainId === 11155111 && process.env.ETHERSCAN_API_KEY) {
+    console.log("Waiting for block confirmations...");
     await simpleStorage.waitForDeployment(6);
     await verify(simpleStorage.target, []);
   }
+
+  //Retrieve the current value
+  const currentValue = await simpleStorage.retrieve();
+  console.log("Current value:", currentValue.toString());
+
+  //Update the current value
+  const transactionResponse = await simpleStorage.store(13);
+  await transactionResponse.wait(1);
+  const updatedValue = await simpleStorage.retrieve();
+  console.log("Updated value:", updatedValue.toString());
 }
 
 async function verify(contracAddress, args) {
